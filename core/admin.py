ec9a201260db3fce93242767e4ea4ca0a8d5321d
sunkanmyt <adesunkanmy@gmail.com>
2026-01-30 01:47:05 +0100

    Feat: Implemented Partner and PartnerTier models with ForeignKey relationship

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Sermon, Event, Milestone
+from .models import Sermon, Event, Milestone, ContactMessage, Partnertier, Partner
 
 admin.site.register(Sermon)
 admin.site.register(Event)
 admin.site.register(Milestone)
+admin.site.register(ContactMessage)
+admin.site.register(Partnertier)
+admin.site.register(Partner)
--- core/models.py
+++ core/models.py
@@ -43,6 +43,23 @@ class ContactMessage(models.Model):
     def __str__(self):
         return f"Message from {self.name} - {self.subject}"
 
-class Partnership(models.Model):
-    title = models.CharField(max_length=255)
+
+class Partnertier(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    minimum_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Partner(models.Model):
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField()
+    tier = models.ForeignKey(Partnertier, on_delete = models.SET_NULL, null=True)
+    joined_on = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.fullname
     
