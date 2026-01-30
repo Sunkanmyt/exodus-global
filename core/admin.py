e00a3630719eec09622b340d43721309644e974d
sunkanmyt <adesunkanmy@gmail.com>
2026-01-30 01:19:14 +0100

    Feat: Added ContactMessage model and REST Framework setup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Sermon, Event, Milestone
 
-# Register your models here.
+admin.site.register(Sermon)
+admin.site.register(Event)
+admin.site.register(Milestone)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Sermon(models.Model):
+    title = models.CharField(max_length=255)
+    speaker = models.CharField(max_length=100)
+    date_preached = models.DateField()
+    description = models.TextField(blank=True, null=True) 
+    video_url = models.URLField(blank=True, null=True) 
+    transcript = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_time = models.DateTimeField() 
+    location = models.CharField(max_length=255, default="Online")
+    registration_link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} on {self.start_time}"
+
+
+class Milestone(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='milestones/')
+    description = models.TextField(blank=True)
+    date_achieved = models.DateField()
+
+    def __str__(self):
+        return self.title
+    
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    received_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} - {self.subject}"
+
+class Partnership(models.Model):
+    title = models.CharField(max_length=255)
+    
