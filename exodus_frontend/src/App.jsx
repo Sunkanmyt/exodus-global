88011b7ed9bf74c1d7045279d83f707c8b4e50ee
sunkanmyt <adesunkanmy@gmail.com>
2026-02-02 06:26:32 +0100

    Feat: Implemented MainLayout, Navbar, and React Router navigation

--- exodus_frontend/src/App.jsx
+++ exodus_frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import Sermons from "./pages/Sermons";
+import { BrowserRoute, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Sermons from '.pages/Sermons';
+
+const Home = () => <div className="p-20 text-center text-4xl font-bold">Welcome to Exodus Global</div>;
+const Gallery = () => <div className="p-20 text-center text-4xl font-bold">Gallery Coming Soon</div>;
 
 function App() {
   return (
-    <div>
-      <Sermons/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* The Layout wraps ALL these routes */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="sermons" element={<Sermons />} />
+          <Route path="gallery" element={<Gallery />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
